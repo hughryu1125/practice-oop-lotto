ddd00adad54f691c2634e303c82b3bc8725328e2
hughryu1125 <hughr1125@gmail.com>
2025-09-29 00:54:32 +0900

    기능만 구현한 초기 버전입니다.... 프로그래밍 요구 사항들을 다 반영시키지 못했습니다...

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
 public class Application {
+
     public static void main(String[] args) {
+
+        FlowManager flowManager = new FlowManager();
         // TODO: 프로그램 구현
+        flowManager.Start();
+
     }
 }
